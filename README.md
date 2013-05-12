cbbf064d3a54bbfb55f8f4c0a4e57283f594cd43
sam <sam@ssaa.(none)>
2013-05-12 22:37:38 +1200

    Rename to jubjub and remove twitter stuff

--- README.md
+++ README.md
@@ -1,30 +1,10 @@
-# teabreakbot #
+# jubjub #
 
-teabreakbot is an advanced software solution to co-ordinatinating the essential task of regular teabreaks.
-
-Supported Features Include:
-
- * Extensible teabreak types including:
-  * teabreaks
-  * lunchbreaks
-  * beerbreaks
- * Post teabreaks to twitter
- * Cross Platform (Linux tested)
- * Access Controls over the ability to call teabreaks
- * Tools to obtain a list of teabreaks from a twitter account (useful if you run multiple decentralised copies of teabreakbot)
- * Teabreak logging for data analysis
-
- * Plus many more planned features including
-  * An advanced centralised management system
-  * Visualisation platform for teabreaks
-  * Natural language support
-  * RESTful API
-  * Automatic Updating
+An irc bot, forked from teabreakbot http://www.github.com/gizmoguy/teabreakbot
 
 ## Requirements ##
 
 * Python 2.6 or later
 * Twisted: http://twistedmatrix.com
-* Tweepy: http://tweepy.github.com
 * Requests: http://python-requests.org
 * An IRC server to operate in
--- jubjubconfig.py
+++ jubjubconfig.py
@@ -2,43 +2,28 @@ import ConfigParser
 import os
 
 def read_config():
-    filenames = ['/etc/teabreakbot.cfg',
-            os.path.expanduser('~/.teabreakbot.cfg'),
-            'teabreakbot.cfg']
+    filenames = ['/etc/jubjub.cfg',
+            os.path.expanduser('~/.jubjub.cfg'),
+            'jubjub.cfg']
 
-    config=ConfigParser.SafeConfigParser()
+    config = ConfigParser.SafeConfigParser()
 
     config.add_section("irc")
+    config.set("irc","nickname","jubjub")
+    config.set("irc","username","jubjub")
     config.set("irc","host","localhost")
     config.set("irc","port","6667")
     config.set("irc","chan","#teabreakbot")
 
-    config.add_section("twitter")
-    config.set("twitter","enabled","false")
-    config.set("twitter","screen_name","")
-    config.set("twitter","consumer_key","")
-    config.set("twitter","consumer_secret","")
-    config.set("twitter","access_key","")
-    config.set("twitter","access_secret","")
-
     config.add_section("log")
     config.set("log", "enabled","false")
-    config.set("log", "path", "~/.teabreakbot.log")
+    config.set("log", "path", "~/.jubjub.log")
 
     config.add_section("misc")
     config.set("misc","teabreaklength","600")
 
     config.read(filenames)
-    return config
 
-if __name__ == "__main__":
-    config = read_config()
-    print "Tweet teabreaks: %s" % config.get("twitter","enabled")
-    print "Twitter Consumer Key: %s" % config.get("twitter","consumer_key")
-    print "Twitter Consumer Secret: %s" % config.get("twitter","consumer_secret")
-    print "Twitter Access Key: %s" % config.get("twitter","access_key")
-    print "Twitter Access Secret: %s" % config.get("twitter","access_secret")
-    if config.getboolean("log","enabled"):
-        print "Logging to: %s" % config.get("log", "path")
+    return config
 
 # vim: set smartindent shiftwidth=4 tabstop=4 softtabstop=4 expandtab :
