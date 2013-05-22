e5609f9491018076c8f5c232c35bad87f34ab7e7
Sam Weston <sam@weston.net.nz>
2013-05-22 21:33:44 +1200

    actually push changes... wtfs, and add .gitignore

--- modules/template.py
+++ modules/template.py
@@ -4,24 +4,22 @@ class JubJubModule():
     def __init__(self, bot):
         print 'Template init()'
         self.bot = bot
-
-    def pre_load_config(self, bot):
-        print 'Template pre_load_config()'
-
-    def post_load_config(self, bot):
-        print 'Template post_load_config()'
+        # handle configuration
 
     def on_connect(self):
         print 'Template on_connect()'
 
-    def on_message(self, user, channel, msg):
+    def on_chan_join(self, channel):
+        print 'Template on_chan_join()'
+
+    def on_message(self, username, channel, msg):
         print 'Template on_message()'
 
-    def on_directed_message(self, user, channel, msg):
+    def on_directed_message(self, username, target, channel, msg):
         print 'Template on_directed_message()'
 
-    def on_user_join(self, user, channel):
+    def on_user_join(self, username, channel):
         print 'Template on_user_join()'
 
-    def on_user_leave(self, user, channel):
+    def on_user_leave(self, username, channel):
         print 'Template on_user_leave()'
