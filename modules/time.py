7e80f6fa59e1a25a22cb9c4b7031d5770c85529f
Sam Weston <sam@weston.net.nz>
2013-05-23 19:31:36 +1200

    clean up

--- modules/time.py
+++ modules/time.py
@@ -16,18 +16,22 @@ class JubJubModule():
         args = msg.split(' ')
         if len(args) > 0 and args[0] == '!timeat':
             location = msg[len(args[0])+1:]
-            r1 = requests.get('http://maps.googleapis.com/maps/api/geocode/xml?address=' + location + '&sensor=false')
-            xml = ET.fromstring(r1.text.encode('utf-8').strip())
-            if xml.find('status').text == 'OK':
-                xml = xml.find('result')
-                actual_location = xml.find('formatted_address').text
-                coord = xml.find('geometry').find('location')
-                r2 = requests.get('https://maps.googleapis.com/maps/api/timezone/xml?location=' + coord[0].text + ',' + coord[1].text + '&timestamp=1331161200&sensor=true')
-                timezone = ET.fromstring(r2.text.encode('utf-8').strip()).find('time_zone_id').text
-                the_time = pytz.utc.localize(datetime.datetime.utcnow()).astimezone(pytz.timezone(timezone))
-                self.bot.say(channel, 'The time in ' + actual_location + ' is ' + the_time.strftime('%X %Z on %b %d, %Y'))
-            else:
-                self.bot.say(channel, 'Sorry could not process that request')
+            try:
+                r1 = requests.get('http://maps.googleapis.com/maps/api/geocode/xml?address=' + location + '&sensor=false')
+                xml = ET.fromstring(r1.text.encode('utf-8').strip())
+                if xml.find('status').text == 'OK':
+                    xml = xml.find('result')
+                    actual_location = xml.find('formatted_address').text
+                    coord = xml.find('geometry').find('location')
+                    r2 = requests.get('https://maps.googleapis.com/maps/api/timezone/xml?location=' + coord[0].text + ',' + coord[1].text + '&timestamp=1331161200&sensor=false')
+                    timezone = ET.fromstring(r2.text.encode('utf-8').strip()).find('time_zone_id').text
+                    the_time = pytz.utc.localize(datetime.datetime.utcnow()).astimezone(pytz.timezone(timezone))
+                    self.bot.say(channel, 'The time in ' + actual_location + ' is ' + the_time.strftime('%X %Z on %b %d, %Y'))
+                else:
+                    self.bot.say(channel, 'Sorry could not process that request')
+            except Exception as e:
+                #something bad happened
+                print 'Exception in time: ', e
 
 
 
